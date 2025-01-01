7a4320aaca14498993bd4f2a80e04fc9867e2678
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-01 23:16:50 +0545

    flashcard routes

---
+++ routes/flashcardrouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { 
+    createFlashcardActivity,
+    getAllFlashcardActivities,
+    getFlashcardActivityById,
+    updateFlashcardActivity,
+    deleteFlashcardActivity
+} = require('../controller/flashcardController');
+
+router.get('/flashcard', getAllFlashcardActivities);
+router.get('/flashcard/:id', getFlashcardActivityById);
+router.post('/flashcard', createFlashcardActivity);
+router.put('/flashcard/:id', updateFlashcardActivity);
+router.delete('/flashcard/:id', deleteFlashcardActivity);
+
+module.exports = router;
