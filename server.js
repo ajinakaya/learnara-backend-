1e633f37a37a059dc1eb5888c7eba8ae33be7433
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-02 00:07:08 +0545

    router

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config({ path: './config/.env' });
 const { mongoose } = require('mongoose')
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
@@ -10,19 +9,26 @@ const app = express();
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('Database connected'))
     .catch((err) => console.log('Database not connected', err))
+    
 
 // middleware
 app.use(express.json())
 app.use(cookieParser());
 
 
-
+// routes
 app.use('/', require('./routes/authroutes')),//Authentication routes
 app.use('/preferred-language', require('./routes/preferredLanguageRouter')); 
 app.use('/language', require('./routes/languageselectedRoute'));
 app.use('/learning-goal', require('./routes/learningGoalRouter'));
 app.use('/set-goal', require('./routes/setGoalRouter'));
-
+app.use('/quiz', require('./routes/quizrouter'));
+app.use('/chapter', require('./routes/chapterrouter'));
+app.use('/audio', require('./routes/audiorouter'));
+app.use('/video', require('./routes/videorouter'));
+app.use('/lesson', require('./routes/sublessonrouter'));
+app.use('/flashcard', require('./routes/flashcardrouter'));
+app.use('/courser', require('./routes/courserouter'));   
 
 
 
