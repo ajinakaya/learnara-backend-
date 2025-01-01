26d98907ebf1a5bc43f673df2f9de6a6d38aff91
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-01 22:05:36 +0545

    audioController

---
+++ controller/audioController.js
@@ -0,0 +1,64 @@
+const AudioActivity = require('../models/audio');
+
+// Create an Audio Activity
+const createAudioActivity = async (req, res) => {
+  try {
+    const newAudioActivity = await AudioActivity.create(req.body);
+    res.status(201).json(newAudioActivity);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+// Get all Audio Activities
+const getAllAudioActivities = async (req, res) => {
+  try {
+    const activities = await AudioActivity.find();
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get a specific Audio Activity by ID
+const getAudioActivityById = async (req, res) => {
+  try {
+    const activity = await AudioActivity.findById(req.params.id);
+    if (!activity) return res.status(404).json({ error: "Audio Activity not found" });
+    res.status(200).json(activity);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update an Audio Activity
+const updateAudioActivity = async (req, res) => {
+  try {
+    const updatedActivity = await AudioActivity.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedActivity) return res.status(404).json({ error: "Audio Activity not found" });
+    res.status(200).json(updatedActivity);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Delete an Audio Activity
+const deleteAudioActivity = async (req, res) => {
+  try {
+    const deletedActivity = await AudioActivity.findByIdAndDelete(req.params.id);
+    if (!deletedActivity) return res.status(404).json({ error: "Audio Activity not found" });
+    res.status(200).json({ message: "Audio Activity deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+    createAudioActivity,
+    getAllAudioActivities,
+    getAudioActivityById,
+    updateAudioActivity,
+    deleteAudioActivity
+  };
+
