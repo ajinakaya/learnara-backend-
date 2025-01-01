3dee3b283cab554aa38cbad5f5481f34894f6c75
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-02 00:04:03 +0545

    courserouter

---
+++ routes/courserouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { 
+    createCourse,
+    getAllCourses,
+    getCourseById,
+    updateCourse,
+    deleteCourse
+    } = require('../controller/courseController');
+
+router.get('/course', getAllCourses);
+router.get('/course/:id', getCourseById);
+router.post('/course', createCourse);
+router.put('/course/:id', updateCourse);
+router.delete('/course/:id', deleteCourse);
+
+module.exports = router;
