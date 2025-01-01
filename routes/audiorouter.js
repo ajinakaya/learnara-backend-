d7b27fa0888ab58eefe4116aa6a048354eab7e44
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-01 22:23:09 +0545

    audiorouter

---
+++ routes/audiorouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+const {
+    createAudioActivity,
+    getAllAudioActivities,
+    getAudioActivityById,
+    updateAudioActivity,
+    deleteAudioActivity
+} = require('../controller/audioController');
+
+
+router.post('/audio', createAudioActivity);
+router.get('/audio', getAllAudioActivities);
+router.get('/audio/:id', getAudioActivityById);
+router.put('/audio/:id', updateAudioActivity);
+router.delete('/audio/:id', deleteAudioActivity);
+
+module.exports = router;
