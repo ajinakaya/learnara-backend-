1cd7e4069109cca747cfe62100235f61e5781ffa
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-01 23:17:14 +0545

    quiz routes

---
+++ routes/quizrouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  createQuizActivity,
+  getAllQuizActivities,
+  getQuizActivityById,
+  updateQuizActivity,
+  deleteQuizActivity
+} = require('../controller/quizController');
+
+
+// Routes
+router.get('/quiz', getAllQuizActivities);
+router.get('/quiz/:id', getQuizActivityById);
+router.post('/quiz', createQuizActivity);
+router.put('/quiz/:id', updateQuizActivity);
+router.delete('/quiz/:id', deleteQuizActivity);
+
+module.exports = router;
