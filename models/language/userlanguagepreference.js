84edd4485172e4bda9811c19f201d55ff2e48a79
ajinakaya <ajina.kayastha5@gmail.com>
2025-02-26 23:51:41 +0545

    userlanguage

--- models/language/userlanguagepreference.js
+++ models/language/userlanguagepreference.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserLanguagePreferenceSchema = new mongoose.Schema({
+  _id: { 
+    type: String,  // Using String to match languageId type
+    required: true,
+  },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
